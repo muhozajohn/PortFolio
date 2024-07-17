30ebe3f544f0a7153fbe9aa6ab776c6a52626cc7
Muhoza John <87951014+muhozajohn@users.noreply.github.com>
2024-07-17 11:01:41 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="flex flex-col gap-4 justify-center w-full text-white mt-48">
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-4">
-        <div className="w-full col-span-1 h-full md:max-h-[85vh]  bg-color-bg-variant p-2 rounded-md">
+        <div className="w-full col-span-1 h-full md:max-h-fit  bg-color-bg-variant p-2 rounded-md">
           <Me />
         </div>
         <div className="w-full col col-span-1 sm:col-span-1 lg:col-span-3">
